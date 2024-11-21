5191f0df53e9862870c7f7b4667a0ed78f354e5e
Artem Melnykov <artemmelnykov29@gmail.com>
2024-11-21 15:09:51 +0100

    Deleted unnecessary files

---
+++ Mobile-Architecture-KnowledgeIsPower/Assets/CodeBase/Hero/HeroMove.cs
@@ -0,0 +1,70 @@
+﻿using CodeBase.Data;
+using CodeBase.Infrastructure.Services;
+using CodeBase.Infrastructure.Services.Input;
+using CodeBase.Infrastructure.Services.PersistentProgress;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace CodeBase.Hero
+{
+    public class HeroMove : MonoBehaviour, ISavedProgress
+    {
+        public CharacterController CharacterController;
+        public float MovementSpeed;
+
+        private IInputService _inputService;
+        private Camera _camera;
+
+        void Awake()
+        {
+            _inputService = ServiceLocator.Container.Single<IInputService>();
+        }
+
+        void Start()
+        {
+            _camera = Camera.main;
+        }
+
+        void Update()
+        {
+            Vector3 movementVector = Vector3.zero;
+
+            if (_inputService.Axis.sqrMagnitude > Constants.Epsilon)
+            {
+                movementVector = _camera.transform.TransformDirection(_inputService.Axis);
+                movementVector.y = 0f;
+                movementVector.Normalize();
+
+                transform.forward = movementVector;
+            }
+
+            movementVector += Physics.gravity;
+            CharacterController.Move(movementVector * Time.deltaTime * MovementSpeed);
+        }
+
+        public void UpdateProgress(PlayerProgress playerProgress) => 
+            playerProgress.WorldData.PositionOnLevel = new PositionOnLevel(CurrentLevel(), transform.position.AsVectorData());
+
+        public void LoadProgress(PlayerProgress playerProgress)
+        {
+            if (CurrentLevel() == playerProgress.WorldData.PositionOnLevel.Level)
+            {
+                Vector3Data savedPosition = playerProgress.WorldData.PositionOnLevel.Position;
+                if (savedPosition != null)
+                {
+                    Warp(savedPosition);
+                }
+            }
+        }
+
+        private void Warp(Vector3Data savedPosition)
+        {
+            CharacterController.enabled = false;
+            transform.position = savedPosition.AsUnityVector().AddY(CharacterController.height);
+            CharacterController.enabled = true;
+        }
+
+        private static string CurrentLevel() => 
+            SceneManager.GetActiveScene().name;
+    }
+}
---
+++ Mobile-Architecture-KnowledgeIsPower/Assets/CodeBase/Infrastructure/SceneLoader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace CodeBase.Infrastructure
+{
+    public class SceneLoader : GameBootstrapper
+    {
+        private readonly ICoroutineRunner _coroutineRunner;
+
+        public SceneLoader(ICoroutineRunner coroutineRunner)
+        {
+            _coroutineRunner = coroutineRunner;
+        }
+
+        public void Load(string sceneName, Action onLoaded = null) => 
+            _coroutineRunner.StartCoroutine(LoadScene(sceneName, onLoaded));
+
+        private IEnumerator LoadScene(string nextScene, Action onLoaded = null)
+        {
+            if (SceneManager.GetActiveScene().name == nextScene)
+            {
+                onLoaded?.Invoke();
+                yield break;
+            }
+
+            AsyncOperation waitNextScene = SceneManager.LoadSceneAsync(nextScene);
+
+            while (!waitNextScene.isDone)
+            {
+                // Loading screen
+
+                yield return null;
+            }
+
+            onLoaded?.Invoke();
+        }
+    }
+}
